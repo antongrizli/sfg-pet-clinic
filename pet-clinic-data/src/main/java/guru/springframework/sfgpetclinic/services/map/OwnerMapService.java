814fd4d1fe7cd71a9c6f19d44e6903526cf5e8d9
Anton Konikov <antongrizli@gmail.com>
2019-01-21 17:36:21 +0100

    Added CRUD test for OwnerMapService. Closes #49.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -50,7 +50,7 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                         throw new RuntimeException("Pet type is required!");
                     }
 
-                    if(pet.getId() ==  null){
+                    if (pet.getId() == null) {
                         Pet savedPet = petService.save(pet);
                         pet.setId(savedPet.getId());
                     }
@@ -70,6 +70,9 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner findByLastName(String lastName) {
-        return null;
+
+        return this.findAll().stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst().orElse(null);
     }
 }
